1863955ae37170bc18739d864a79376600c49c12
lujanfernaud <lujanfernaud@gmail.com>
2017-12-11 14:12:29 +0000

    Refactor signup_spec to not hit the database

--- spec/signup_spec.rb
+++ spec/signup_spec.rb
@@ -4,22 +4,40 @@ require "signup"
 describe Signup do
   describe "#save" do
     it "creates an account with one user" do
-      signup = Signup.new(email: "user@example.com", account_name: "Example")
+      email        = "user@example.com"
+      account_name = "Example"
+      account      = double("example_account", name: account_name)
 
+      expect(Account).to receive(:create!)
+        .with(name: account_name)
+        .and_return(account)
+
+      expect(User).to receive(:create!)
+        .with(account: account, email: email)
+
+      signup = Signup.new(email: email, account_name: account_name)
       result = signup.save
 
-      expect(Account.count).to eq(1)
-      expect(Account.last.name).to eq("Example")
-      expect(User.count).to eq(1)
-      expect(User.last.email).to eq("user@example.com")
-      expect(User.last.account).to eq(Account.last)
       expect(result).to be(true)
     end
   end
 
   describe "#user" do
     it "returns the user created by #save" do
-      signup = Signup.new(email: "user@example.com", account_name: "Example")
+      email        = "user@example.com"
+      account_name = "Example"
+      account      = double("example_account", name: account_name)
+      user         = double("user", email: email, account: account)
+
+      expect(Account).to receive(:create!)
+        .with(name: account_name)
+        .and_return(account)
+
+      expect(User).to receive(:create!)
+        .with(account: account, email: email)
+        .and_return(user)
+
+      signup = Signup.new(email: email, account_name: account_name)
       signup.save
 
       result = signup.user
