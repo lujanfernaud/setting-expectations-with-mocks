8bf6bd914788eeb182521f0cb1c44dcea707c301
lujanfernaud <lujanfernaud@gmail.com>
2017-12-11 14:32:30 +0000

    Refactor signup_spec

--- spec/signup_spec.rb
+++ spec/signup_spec.rb
@@ -8,12 +8,8 @@ describe Signup do
       account_name = "Example"
       account      = double("example_account", name: account_name)
 
-      expect(Account).to receive(:create!)
-        .with(name: account_name)
-        .and_return(account)
-
-      expect(User).to receive(:create!)
-        .with(account: account, email: email)
+      mock_account_creation_with(account, account_name)
+      mock_user_creation_with(account, email)
 
       signup = Signup.new(email: email, account_name: account_name)
       result = signup.save
@@ -29,13 +25,8 @@ describe Signup do
       account      = double("example_account", name: account_name)
       user         = double("user", email: email, account: account)
 
-      expect(Account).to receive(:create!)
-        .with(name: account_name)
-        .and_return(account)
-
-      expect(User).to receive(:create!)
-        .with(account: account, email: email)
-        .and_return(user)
+      mock_account_creation_with(account, account_name)
+      mock_user_creation_with(account, email).and_return(user)
 
       signup = Signup.new(email: email, account_name: account_name)
       signup.save
@@ -45,5 +36,16 @@ describe Signup do
       expect(result.email).to eq("user@example.com")
       expect(result.account.name).to eq("Example")
     end
+  end
+
+  def mock_account_creation_with(account, account_name)
+    expect(Account).to receive(:create!)
+      .with(name: account_name)
+      .and_return(account)
+  end
+
+  def mock_user_creation_with(account, email)
+    expect(User).to receive(:create!)
+      .with(account: account, email: email)
   end
 end
